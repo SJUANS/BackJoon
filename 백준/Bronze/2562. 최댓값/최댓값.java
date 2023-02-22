aeeb21d940aca18304def7bd2d95bf2252cc22fb
SJUANS <99381863+SJUANS@users.noreply.github.com>
2023-02-22 23:54:43 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -2,29 +2,20 @@ import java.util.*;
 
 public class Main {
     public static void main (String[] args){
-       Scanner sc = new Scanner(System.in);
-       List<Integer> numbers = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+        int[] numbers = new int[9];
+        int index = 0;
+
+        for (int i=0; i<9; i++){
+            numbers[i] = sc.nextInt();
+            max = Math.max(max, numbers[i]);
+            if (max == numbers[i]){
+                index = i + 1;
+            }
+        }
         
-       for (int j=0; j<9; j++) {
-          numbers.add(sc.nextInt()); 
-       }
-       
-       Iterator<Integer> it = numbers.iterator();
-       int max = 0;
-       int index = -1;
-       
-       while (it.hasNext()){
-           int num = it.next(); 
-           max = Math.max(num, max);
-       }
-       
-       for (int i=0; i<9; i++){
-           if (numbers.get(i) == max){
-               index = i+1;
-           }
-       }
-        
-       System.out.println(max);
-       System.out.println(index);
+        System.out.println(max);
+        System.out.println(index);
     }
 }
